7a8f257cb2ac7a441f0d93b50d30580fc3b1859f
Fangxiang Zou <fxzou@thoughtworks.com>
2018-09-19 21:27:27 +0800

    add integration test create shopping cart

---
+++ src/main/java/com/twsz/storeserver/shoppingcart/ShoppingCartItem.java
@@ -0,0 +1,16 @@
+package com.twsz.storeserver.shoppingcart;
+
+import com.twsz.storeserver.product.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ShoppingCartItem {
+    private Product product;
+}
---
+++ src/test/java/com/twsz/storeserver/shoppingcart/ShoppingCartTest.java
@@ -0,0 +1,46 @@
+package com.twsz.storeserver.shoppingcart;
+
+import com.twsz.storeserver.product.Product;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+public class ShoppingCartTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    public void should_return_status_code_CREATED_when_call_create_shopping_cart() {
+        ResponseEntity<Void> responseEntity = restTemplate
+                .postForEntity("/api/shopping-cart-items", mockShoppingCartItems(), Void.class);
+        Assert.assertEquals(responseEntity.getStatusCode(), HttpStatus.CREATED);
+    }
+
+    private List<ShoppingCartItem> mockShoppingCartItems() {
+        return Arrays.asList(
+                ShoppingCartItem.builder()
+                        .product(Product.builder().id(1).build())
+                        .build(),
+                ShoppingCartItem.builder()
+                        .product(Product.builder().id(2).build())
+                        .build()
+        );
+    }
+}
